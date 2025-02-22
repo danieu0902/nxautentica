d9442c58298036520c3a15dc020a4a71fed7b447
Daniel E. Serrano Portillo <danieu275@gmail.com>
2025-02-22 01:16:15 +0100

    21-02-2025

---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+function PaginaAbout() {
+    return (
+      <h1 className="text-3xl font-bold">
+        Página About
+      </h1>
+    )
+  }
+  
+  export default PaginaAbout
--- src/auth.js
+++ src/auth.js
@@ -3,10 +3,21 @@ import Google from '@auth/core/providers/google'
 import Github from '@auth/core/providers/github'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import prisma from '@/lib/prisma'
+import { getUserByEmail, getUserById } from './lib/data'
+import Credentials from '@auth/core/providers/credentials'
 
+
+const credentialsConfig = {
+    // LOGIN ACTION -> AUTHORIZE -> JWT -> SESSION
+    async authorize(credentials) {
+        const user = await getUserByEmail(credentials.email)
+        return user  
+    },
+}
 
 const options = {
    providers: [
+    Credentials(credentialsConfig),
     Google,
     Github
    ],
@@ -15,7 +26,24 @@ const options = {
    pages: {
         signIn: '/login',
         signOut: '/logout',        
-   }
+   },
+   callbacks: {
+    async jwt({ token }) {
+        if (!token.sub) return token;
+
+        const user = await getUserById(token.sub)
+        if (!user) return token;
+
+        token.role = user?.role
+        return token
+    },
+
+    async session({ session, token }) {
+        session.user.id = token?.sub
+        session.user.role = token?.role
+        return session
+    }
+}
 
 }
 
